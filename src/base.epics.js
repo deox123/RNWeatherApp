2caec9d98299f6d41b30f8f6a28ff5b5931e27b8
Selena Gacic <senagacic@gmail.com>
2018-11-05 19:29:12 +0100

    boilerplate code (epics and reducers)

--- src/base.epics.js
+++ src/base.epics.js
@@ -1,10 +1,13 @@
 import {combineEpics} from 'redux-observable';
 
+import HomeEpics from './routes/HomeRoute/epics';
+
+
 /**
  * Combines all the epics together, using the method provided by redux-observable.
  */
 const epics = combineEpics(
-
+    HomeEpics,
 );
 
 export default epics;
--- src/base.reducers.js
+++ src/base.reducers.js
@@ -4,11 +4,13 @@ import {combineReducers} from 'redux';
 import type {CombinedReducer} from 'redux';
 import type {CombinedState, Action} from './index.types';
 
+import HomeRoute from './routes/HomeRoute/reducers';
+
 /**
  * Combines all of our reducers, using the method imported from the 'redux' package.
  */
 const reducers: CombinedReducer<CombinedState, Action> = combineReducers({
-
+    HomeRoute,
 });
 
 export default reducers;
