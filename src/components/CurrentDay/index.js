5ffe9215b1e8bd01813b54f7bec592e5b773001a
deox123 <nikolamilosevic93@yahoo.com>
2018-11-06 19:30:24 +0100

    styling currentDay component

--- src/components/CurrentDay/index.js
+++ src/components/CurrentDay/index.js
@@ -3,12 +3,14 @@
 // flow-disable-next-line
 import * as React from 'react';
 // flow-disable-next-line
-import {Text, View, TouchableHighlight} from 'react-native';
+import {Text, View, TouchableHighlight, Image} from 'react-native';
 import {format, isToday} from 'date-fns';
 
 import styles from './styles';
 
 import type {Props} from './types';
+
+const icon = require('../../../images/partly_sunny.png');
 
 const Header = (props: Props) => {
     const {
@@ -21,6 +23,7 @@ const Header = (props: Props) => {
         sunset_time,
         morning_temperature,
         temperature,
+        // descritpion_flag,
     } = props.weather;
 
     const renderTitle = (d: string): string => {
@@ -30,25 +33,48 @@ const Header = (props: Props) => {
         return title.toUpperCase();
     };
 
+    // let icon;
+    // switch(descritpion_flag) {
+    //     case 'partly_sunny':
+    //         icon = require('images/partly_sunny');
+    //         break;
+
+    //     case 'rain':
+    //         icon = require('./my-icon-active.png');
+    // }
+
     return (
         <View style={styles.container}>
             <View style={styles.main}>
-                <Text>{renderTitle(date)}</Text>
-            </View>
-            <View style={styles.sub}>
-                <View style={styles.left}>
-                    <Text>ReelFeel: {real_feel}</Text>
-                    <Text>Precipitation: {precipitation}%</Text>
-                    <Text>Max UV Index: {max_uv_index}</Text>
-                    <Text>Rain: {rain}mm</Text>
+                <Text style={styles.heading}>{renderTitle(date)}</Text>
+                <View style={styles.mainColumns}>
+                    <View style={styles.mainLeft}>
+                        <View style={styles.temperatures}>
+                            <Text style={styles.maxTemp}>{temperature}°</Text>
+                            <Text style={styles.minTemp}>/ {morning_temperature}°</Text>
+                        </View>
+                        <Text style={styles.description}>Partly sunny</Text>
+                    </View>
+                    <Image
+                        source={icon}
+                        // style={styles.icon}
+                    />
                 </View>
-                <View styles={styles.right}>
-                    <Text>Sunrize {sunrise_time} AM</Text>
-                    <Text>Sunset {sunset_time} PM</Text>
+            </View>
+            <View style={styles.details}>
+                <View style={styles.detailsLeft}>
+                    <Text style={styles.detailsText}>ReelFeel: {real_feel}°</Text>
+                    <Text style={styles.detailsText}>Precipitation: {precipitation}%</Text>
+                    <Text style={styles.detailsText}>Max UV Index: {max_uv_index}</Text>
+                    <Text style={styles.detailsText}>Rain: {rain}mm</Text>
+                </View>
+                <View style={styles.detailsRight}>
+                    <Text style={styles.detailsText}><Text style={styles.grayText}>Sunrize:</Text> {sunrise_time} AM</Text>
+                    <Text style={styles.detailsText}><Text style={styles.grayText}>Sunset:</Text> {sunset_time} PM</Text>
                 </View>
             </View>
             <View style={styles.button}>
-                <Text>SEE HOURLY</Text>
+                <Text style={styles.buttonText}>SEE HOURLY</Text>
             </View>
         </View>
     );
--- src/components/CurrentDay/styles.js
+++ src/components/CurrentDay/styles.js
@@ -1,9 +1,11 @@
-import createStyles, {colors, baseStyles, fonts} from '../../base.styles';
+import createStyles, {colors, dimensions, fonts} from '../../base.styles';
 
 const HeaderStyles = createStyles({
     container: {
         flex: 2 / 3,
-        backgroundColor: 'red',
+        // backgroundColor: 'red',
+        padding: dimensions.md,
+        paddingBottom: 0,
     },
     text: {
         fontSize: fonts.lg,
@@ -11,19 +13,75 @@ const HeaderStyles = createStyles({
         fontWeight: '600',
         paddingLeft: 20,
     },
+    heading: {
+        color: colors.orange,
+        fontWeight: '500',
+        fontSize: fonts.md,
+    },
     main: {
         flex: 3,
     },
-    sub: {
+    mainColumns: {
+        flexDirection: 'row',
+    },
+    mainLeft: {
+        flex: 1,
+    },
+    temperatures: {
+        flexDirection: 'row',
+        paddingVertical: 10,
+    },
+    maxTemp: {
+        fontSize: fonts.xxxl,
+        color: colors.black,
+        paddingRight: 10,
+    },
+    minTemp: {
+        fontSize: fonts.xxl,
+        color: colors.gray,
+    },
+    description: {
+        fontSize: fonts.lg,
+        color: colors.gray,
+    },
+    icon: {
+        flex: 1,
+    },
+    details: {
         flex: 2,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        borderStyle: 'solid',
+        borderTopWidth: 0.5,
+        borderBottomWidth: 0.5,
+        borderColor: colors.gray,
+        marginHorizontal: -15,
+        paddingVertical: 3,
     },
-    right: {
-        // alignSelf: 
+    detailsText: {
+        color: colors.black,
+        fontSize: fonts.md,
+        padding: 2,
+        paddingHorizontal: 15,
+    },
+    grayText: {
+        color: colors.gray,
+    },
+    detailsRight: {
+        alignItems: 'flex-end',
     },
     button: {
+        height: 50,
+        alignItems: 'flex-end',
+        alignContent: 'flex-end',
+    },
+    buttonText: {
+        color: colors.blue,
+        fontSize: fonts.md,
+        fontWeight: '400',
+        textAlign: 'right',
         flex: 1,
+        textAlignVertical: 'center',
     },
 });
 
